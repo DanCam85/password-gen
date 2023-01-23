ff7a5f59979de84c3c02525d633bb90382e48969
DanCam85 <96871656+DanCam85@users.noreply.github.com>
2023-01-23 20:11:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,119 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 15;
+let generate = document.getElementById("generate");
+let firstPasswordEl = document.getElementById("first-password");
+let secondPasswordEl = document.getElementById("second-password");
+
+function generateRandomCharacters() {
+  let randomChar = Math.floor(Math.random() * characters.length);
+  return characters[randomChar];
+}
+
+function generateRandomPassword() {
+  let randomPassword = "";
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += generateRandomCharacters();
+  }
+  return randomPassword;
+}
+
+let RandomPasswordOne = generateRandomPassword();
+let RandomPasswordTwo = generateRandomPassword();
+
+function generatePassword() {
+  firstPasswordEl.textContent += RandomPasswordOne;
+  secondPasswordEl.textContent += RandomPasswordTwo;
+}
